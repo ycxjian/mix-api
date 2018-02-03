3dc79ea4c695cbba76935f0590b178b668a91d8d
暗黑但丁 <2879070086@qq.com>
2018-02-03 18:47:57 +0800

    Update index.js

--- routes/novel/index.js
+++ routes/novel/index.js
@@ -1,21 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var superagent = require('superagent');
-const musicAPI = require('music-api');
 
 router.get('/', function(req, res, next) {
   res.send('nihao');
 });
 
-router.get('/search', function(req, res, next) {
-  musicAPI.searchSong('netease', {
-      key: '周杰伦',
-      limit: 10,
-      page: 1,
-    })
-    .then(response => res.send(response))
-    .catch(err => res.send(err))
+router.get('/getNovelByName', function(req, res, next) {
+  // superagent.get('http://api.zhuishushenqi.com/cats/lv2').then((response) => {
+    // res.send(response);
+  // })
+  res.send(req.useragent);
 });
 
 module.exports = router;
-
