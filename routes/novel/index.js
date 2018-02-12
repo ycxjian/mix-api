bbc9921004865d3c0433be39210c9acf5ac316bf
暗黑但丁 <2879070086@qq.com>
2018-02-12 11:16:24 +0800

    Update index.js

--- routes/novel/index.js
+++ routes/novel/index.js
@@ -7,6 +7,14 @@ router.get('/', function(req, res, next) {
   res.send(util.inspect(req));
 });
 
+router.get('/getip', function(req, res, next) {
+  var ip = (req.headers['x-forwarded-for'] ||
+     req.connection.remoteAddress ||
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress).split(",")[0];
+  res.send(ip);
+});
+
 router.get('/getNovelByName', function(req, res, next) {
   // superagent.get('http://api.zhuishushenqi.com/cats/lv2').then((response) => {
     // res.send(response);
