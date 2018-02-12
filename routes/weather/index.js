e9e3789983fd6ee10241397eaa2beddbfec6b5b3
暗黑但丁 <2879070086@qq.com>
2018-02-12 14:33:59 +0800

    Update index.js

--- routes/weather/index.js
+++ routes/weather/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
   res.send('天气');
 });
 
-router.get('/weather',function(req, res, next) {
+router.get('/now',function(req, res, next) {
   const location = req.query.location;
   const weatherApi = new Api('U3816B37F8','p3zpi4nz7b0k0ugd');
   weatherApi.getWeatherNow(location).then((json)=>{
@@ -17,5 +17,36 @@ router.get('/weather',function(req, res, next) {
     res.send(err);
   });
 });
+
+router.get('/now',function(req, res, next) {
+  const location = req.query.location;
+  const weatherApi = new Api('U3816B37F8','p3zpi4nz7b0k0ugd');
+  weatherApi.getWeatherNow(location).then((json)=>{
+    res.send(json);
+  }).catch((err)=>{
+    res.send(err);
+  });
+});
+
+router.get('/future',function(req, res, next) {
+  const location = req.query.location;
+  const weatherApi = new Api('U3816B37F8','p3zpi4nz7b0k0ugd');
+  weatherApi.getFuture(location).then((json)=>{
+    res.send(json);
+  }).catch((err)=>{
+    res.send(err);
+  });
+});
+
+router.get('/life',function(req, res, next) {
+  const location = req.query.location;
+  const weatherApi = new Api('U3816B37F8','p3zpi4nz7b0k0ugd');
+  weatherApi.getLife(location).then((json)=>{
+    res.send(json);
+  }).catch((err)=>{
+    res.send(err);
+  });
+});
+
 
 module.exports = router;
