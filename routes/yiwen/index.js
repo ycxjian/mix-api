a02ac10f1c8f8c33a36b8e7141fed68ca2ef9a40
暗黑但丁 <2879070086@qq.com>
2018-03-26 15:53:12 +0800

    Update index.js

--- routes/yiwen/index.js
+++ routes/yiwen/index.js
@@ -4,10 +4,10 @@ var superagent = require('superagent');
 
 router.get('/:arg', function(req, res, next) {
   var url = 'https://interface.meiriyiwen.com/article/today?dev=1';
-  if (arg === 'random') {
+  if (req.params.arg === 'random') {
     url = "https://interface.meiriyiwen.com/article/random?dev=1";
   }
-  if (arg === 'date') {
+  if (req.params.arg === 'date') {
     url = "https://interface.meiriyiwen.com/article/day?dev=1&date=" + req.query.date;
   }
   superagent.get(url).then((json) => {
