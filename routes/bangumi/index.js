a661808b59f81e48471e8bbd17f1f425cdceeaa3
暗黑但丁 <2879070086@qq.com>
2018-04-03 17:01:57 +0800

    Update index.js

--- routes/bangumi/index.js
+++ routes/bangumi/index.js
@@ -5,15 +5,14 @@ const NodeCache = require("node-cache");
 const myCache = new NodeCache();
 
 router.get('/', function(req, res, next) {
-  try {
-    const jsonData = myCache.get("jsonData", true);
-  } catch( geterr ){
-    superagent
-    .get('https://raw.githubusercontent.com/bangumi-data/bangumi-data/master/dist/data.json')
-    .end(function(err, response) {
-      myCache.set("jsonData", response.text, 86400);
-    });
-  }
+  const jsonData = myCache.get("jsonData");
+    if(jsonData === undefined) {
+      superagent
+      .get('https://raw.githubusercontent.com/bangumi-data/bangumi-data/master/dist/data.json')
+      .end(function(err, response) {
+        myCache.set("jsonData", response.text, 86400);
+      });
+    }
   res.send(jsonData);
 });
 
